41613c09e1a64261245eff85606dd478f2812244
Bernardo <bernardo.zambranoferreira@gmail.com>
2020-10-25 13:35:43 +0100

    IFS & WHILES DONE TODO FUNCTIONS  & VECTOR

--- generacion.c
+++ generacion.c
@@ -4,18 +4,21 @@
 #include "generacion.h"
 
 
-void escribir_subseccion_data(FILE* fpasm){
+void escribir_subseccion_data(FILE* fpasm)
+{
 	fprintf(fpasm, "segment .data\n");
 	fprintf(fpasm, "\t_msg_error_div db  \"Error, división por 0\" \n");
 	fprintf(fpasm, "\t_msg_error_rango db 0\n");
 }
 
-void escribir_cabecera_bss(FILE* fpasm){
+void escribir_cabecera_bss(FILE* fpasm)
+{
 	fprintf(fpasm, "segment .bss\n");
 	fprintf(fpasm, "\t__esp resd 1\n");
 }
 
-void declarar_variable(FILE* fpasm, char * nombre,  int tipo,  int tamano){
+void declarar_variable(FILE* fpasm, char * nombre,  int tipo,  int tamano)
+{
 	if(tipo == ENTERO){
 		fprintf(fpasm, "\t_%s resd %d\n", nombre, tamano);
 	}else{
@@ -33,7 +36,9 @@ void escribir_segmento_codigo(FILE* fpasm){
 	fprintf(fpasm, "\textern scan_int, print_int, scan_float, print_float, scan_boolean, print_boolean, print_endofline, print_blank, print_string, alfa_malloc, alfa_free, ld_float\n");
 
 }
-void escribir_inicio_main(FILE* fpasm) {
+
+void escribir_inicio_main(FILE* fpasm)
+{
 	/*La etiqueta main*/
 	fprintf(fpasm, "main:\n");
 
@@ -48,7 +53,8 @@ fprintf(fpasm, "\tret\n");
 
 }
 
-void escribir_operando(FILE* fpasm, char* nombre, int es_var){
+void escribir_operando(FILE* fpasm, char* nombre, int es_var)
+{
 
 	if (es_var == 0){ /* cuando es 0 hace referencia a un numero*/
 		fprintf(fpasm, "\tmov dword eax, %s\n", nombre);
@@ -58,11 +64,12 @@ void escribir_operando(FILE* fpasm, char* nombre, int es_var){
 	}
 }
 
-void asignar(FILE* fpasm, char* nombre, int es_variable){
+void asignar(FILE* fpasm, char* nombre, int es_variable)
+{
 
 	fprintf(fpasm, "\tpop eax\n");
 
-	if(es_variable==1){
+	if(es_variable>0){
 		fprintf(fpasm, "\tmov eax, [eax]\n");
 	}
 	fprintf(fpasm, "\tmov dword [_%s], eax\n", nombre);
@@ -70,7 +77,8 @@ void asignar(FILE* fpasm, char* nombre, int es_variable){
 }
 
 
-void sumar(FILE* fpasm, int es_variable_1, int es_variable_2){
+void sumar(FILE* fpasm, int es_variable_1, int es_variable_2)
+{
 
 	fprintf( fpasm, "\tpop dword eax\n ");
 	fprintf( fpasm, "\tmov eax, [eax]\n ");
@@ -82,7 +90,8 @@ void sumar(FILE* fpasm, int es_variable_1, int es_variable_2){
 	fprintf(fpasm, "\tpush dword eax\n");
 
 }
-void restar(FILE* fpasm, int es_referencia_1, int es_referencia_2){
+void restar(FILE* fpasm, int es_referencia_1, int es_referencia_2)
+{
 
 	fprintf( fpasm, "\tpop dword eax\n ");
 	fprintf( fpasm, "\tmov eax, [eax]\n ");
@@ -96,18 +105,19 @@ void restar(FILE* fpasm, int es_referencia_1, int es_referencia_2){
 }
 
 
-void multiplicar(FILE* fpasm, int es_variable_1, int es_variable_2){
+void multiplicar(FILE* fpasm, int es_variable_1, int es_variable_2)
+{
 
 	/*Extraemos los argumentos y los preparamos para la multiplicación*/
 
 	fprintf( fpasm, "\tpop dword eax\n ");
 	fprintf( fpasm, "\tpop dword ebx\n ");
 
-	if( es_variable_2==1){
+	if( es_variable_2>0){
     	fprintf( fpasm, "\tmov dword eax, [eax]\n ");
 	}
 
-	if( es_variable_1==1){
+	if( es_variable_1>0){
     	fprintf( fpasm, "\tmov dword ebx, [ebx]\n ");
 	}
 
@@ -118,13 +128,14 @@ void multiplicar(FILE* fpasm, int es_variable_1, int es_variable_2){
 
 }
 
-void dividir(FILE* fpasm, int es_variable_1, int es_variable_2){
+void dividir(FILE* fpasm, int es_variable_1, int es_variable_2)
+{
 
 /* Extraemos los argumentos y los preparamos para la división*/
 
 	fprintf( fpasm, "\tpop dword eax\n");
 
-	if( es_variable_2==1){
+	if( es_variable_2>0){
     	fprintf( fpasm, "\tmov dword eax, [eax]\n");
 	}
 
@@ -132,7 +143,7 @@ void dividir(FILE* fpasm, int es_variable_1, int es_variable_2){
 
 	fprintf( fpasm, "\tpop dword ecx\n");
 
-	if( es_variable_1==1){
+	if( es_variable_1>0){
     	fprintf( fpasm, "\tmov dword ecx, [ecx]\n");
 	}
 
@@ -164,7 +175,8 @@ void dividir(FILE* fpasm, int es_variable_1, int es_variable_2){
 }
 
 
-void o(FILE* fpasm, int es_variable1, int es_variable2){
+void o(FILE* fpasm, int es_variable1, int es_variable2)
+{
 	fprintf(fpasm, "\tpop dword edx\n"); /*extraemos la segunda variable*/
 	if(es_variable2 == 1){
 		fprintf(fpasm, "\tmov dword edx, [edx]\n");  /*comprobamos si la segunda*/
@@ -184,7 +196,8 @@ posición de memoria*/
 }
 
 
-void y(FILE* fpasm, int es_variable1, int es_variable2){
+void y(FILE* fpasm, int es_variable1, int es_variable2)
+{
 	fprintf(fpasm, "\tpop dword edx\n"); /*extraemos la segunda variable*/
 	if(es_variable2 == 1){
 		fprintf(fpasm, "\tmov dword edx, [edx]\n");  /*comprobamos si la segunda*/
@@ -203,7 +216,8 @@ posición de memoria*/
 	fprintf(fpasm, "\tpush dword eax\n");
 }
 
-void cambiar_signo(FILE* fpasm, int es_variable){
+void cambiar_signo(FILE* fpasm, int es_variable)
+{
 
 /*Guardamos la referencia a memoria de nuestra variable en el registro eax*/
 fprintf(fpasm,"\tpop dword eax\n");
@@ -222,7 +236,8 @@ fprintf(fpasm, "\tpush dword eax\n");
 
 }
 
-void no(FILE* fpasm, int es_variable, int cuantos_no){
+void no(FILE* fpasm, int es_variable, int cuantos_no)
+{
 
 fprintf(fpasm, "\tpop eax\n");
 if(es_variable == 1) fprintf(fpasm, "\tmov eax, [eax]\n");
@@ -257,7 +272,8 @@ fprintf(fpasm, "\t_fin_not%d:\n", cuantos_no);
 
 
 
- void distinto(FILE* fpasm, int es_variable1, int es_variable2, int etiqueta){
+ void distinto(FILE* fpasm, int es_variable1, int es_variable2, int etiqueta)
+ {
 	 fprintf( fpasm, "\tpop dword eax\n" );
    fprintf( fpasm, "\tpop dword ebx\n" );
 
@@ -272,7 +288,8 @@ fprintf(fpasm, "\t_fin_not%d:\n", cuantos_no);
  fprintf( fpasm, "\tcmp eax, ebx\n" );
  fprintf( fpasm, "\tjne _%d\n", etiqueta );
  }
- void menor_igual(FILE* fpasm, int es_variable1, int es_variable2, int etiqueta){
+ void menor_igual(FILE* fpasm, int es_variable1, int es_variable2, int etiqueta)
+ {
  fprintf( fpasm, "\tpop dword eax\n" );
  fprintf( fpasm, "\tpop dword ebx\n" );
 
@@ -288,7 +305,8 @@ fprintf(fpasm, "\t_fin_not%d:\n", cuantos_no);
  fprintf( fpasm, "\tjbe _%d\n", etiqueta );
  }
 
- void mayor_igual(FILE* fpasm, int es_variable1, int es_variable2, int etiqueta) {
+ void mayor_igual(FILE* fpasm, int es_variable1, int es_variable2, int etiqueta)
+ {
 
 	 fprintf( fpasm, "\tpop dword eax\n" );
 	 fprintf( fpasm, "\tpop dword ebx\n" );
@@ -306,7 +324,8 @@ fprintf(fpasm, "\t_fin_not%d:\n", cuantos_no);
 
 }
 
-void menor(FILE* fpasm, int es_variable1, int es_variable2, int etiqueta){
+void menor(FILE* fpasm, int es_variable1, int es_variable2, int etiqueta)
+{
 	fprintf(fpasm, "\tpop ebx\n");
 	fprintf(fpasm, "\tpop eax\n");
 
@@ -338,7 +357,8 @@ void mayor(FILE* fpasm, int es_variable1, int es_variable2, int etiqueta){
 }
 
 
-void leer(FILE* fpasm, char* nombre, int tipo){
+void leer(FILE* fpasm, char* nombre, int tipo)
+{
 	fprintf(fpasm, "\tpush dword _%s\n", nombre);
 	if(tipo==BOOLEANO)
 		fprintf(fpasm, "\tcall scan_boolean\n");
@@ -348,7 +368,8 @@ void leer(FILE* fpasm, char* nombre, int tipo){
 	fprintf(fpasm, "\tadd esp, 4\n");
 }
 
-void escribir(FILE* fpasm, int es_variable, int tipo){
+void escribir(FILE* fpasm, int es_variable, int tipo)
+{
 	if(es_variable){
 		fprintf(fpasm, "\tpop eax\n");
 		fprintf(fpasm, "\tmov eax, [eax]\n");
@@ -363,4 +384,71 @@ void escribir(FILE* fpasm, int es_variable, int tipo){
 	fprintf(fpasm, "\tadd esp, 4\n");
 	fprintf(fpasm, "\tcall print_endofline\n");
 
+}
+
+
+
+/*** IFS & WHILE ***/
+void ifthenelse_inicio(FILE * fpasm, int exp_es_variable, int etiqueta)
+{
+
+	fprintf(fpasm, "\tpop eax\n");
+
+	if(exp_es_variable>0){
+		fprintf(fpasm, "\tmov eax, dword [eax]\n");
+		}
+
+	fprintf(fpasm, "\tcmp eax, 0\n");
+	fprintf(fpasm, "\tje near _fin_if%d\n", etiqueta);
+}
+
+void ifthen_inicio(FILE * fpasm, int exp_es_variable, int etiqueta)
+{
+
+	fprintf(fpasm, "\tpop eax\n");
+
+	if(exp_es_variable>0){
+		fprintf(fpasm, "\tmov eax, dword [eax]\n");
+		}
+
+	fprintf(fpasm, "\tcmp eax, 0\n");
+	fprintf(fpasm, "\tje near _fin_if%d\n", etiqueta);
+}
+
+void ifthen_fin(FILE * fpasm, int etiqueta)
+{
+	fprintf(fpasm, "\t_fin_if%d: \n", etiqueta);
+}
+
+void ifthenelse_fin_then( FILE * fpasm, int etiqueta)
+ {
+	 fprintf(fpasm, "\tjmp near _fin_if_else%d: \n", etiqueta);
+	 fprintf(fpasm, "\t_fin_then%d: \n", etiqueta);
+ }
+
+void ifthenelse_fin( FILE * fpasm, int etiqueta)
+{
+	fprintf(fpasm, "\t_fin_if_else%d: \n", etiqueta);
+}
+
+void while_inicio(FILE * fpasm, int etiqueta)
+{
+	fprintf(fpasm, "\t_while_start%d: \n", etiqueta);
+}
+
+void while_exp_pila (FILE * fpasm, int exp_es_variable, int etiqueta)
+{
+	fprintf(fpasm, "\tpop eax \n");
+	if(exp_es_variable>0){
+		fprint(fpasm,"\tmov eax, [eax]\n");
+	}
+	fprintf(fpasm, "\tcmp eax, 0");
+	fprintf(fpasm, "\tje _fin_while%d \n", etiqueta );
+}
+
+
+void while_fin( FILE * fpasm, int etiqueta)
+{
+	fprintf(fpasm, "\t jmp near _while_start%d \n", etiqueta);
+	fprintf(fpasm, "\t_fin_while%d \n", etiqueta );
 }
--- generacion.h
+++ generacion.h
@@ -184,4 +184,88 @@ void leer(FILE* fpasm, char* nombre, int tipo);
 
 
 void escribir(FILE* fpasm, int es_variable, int tipo);
+
+
+/*** IFS & WHILE ***/
+void ifthenelse_inicio(FILE * fpasm, int exp_es_variable, int etiqueta);
+/*
+* Generación de código para el inicio de una estructura if-then-else
+* Como es el inicio de uno bloque de control de flujo de programa que requiere de una nueva
+* etiqueta deben ejecutarse antes las tareas correspondientes a esta situación
+* exp_es_variable
+* Es 1 si la expresión de la condición es algo asimilable a una variable (identificador,
+* elemento de vector)
+* Es 0 en caso contrario (constante u otro tipo de expresión)
+*/
+
+void ifthen_inicio(FILE * fpasm, int exp_es_variable, int etiqueta);
+/*
+* Generación de código para el inicio de una estructura if-then
+* Como es el inicio de uno bloque de control de flujo de programa que requiere de una nueva
+* etiqueta deben ejecutarse antes las tareas correspondientes a esta situación
+* exp_es_variable
+* Es 1 si la expresión de la condición es algo asimilable a una variable (identificador,
+* elemento de vector)
+* Es 0 en caso contrario (constante u otro tipo de expresión)
+*/
+
+void ifthen_fin(FILE * fpasm, int etiqueta);
+/*
+* Generación de código para el fin de una estructura if-then
+* Como es el fin de uno bloque de control de flujo de programa que hace uso de la etiqueta
+* del mismo se requiere que antes de su invocación tome el valor de la etiqueta que le toca
+* según se ha explicado
+* Y tras ser invocada debe realizar el proceso para ajustar la información de las etiquetas
+* puesto que se ha liberado la última de ellas.
+*/
+
+void ifthenelse_fin_then( FILE * fpasm, int etiqueta);
+/* Generación de código para el fin de la rama then de una estructura if-then-else
+* Sólo necesita usar la etiqueta adecuada, aunque es el final de una rama, luego debe venir
+* otra (la rama else) antes de que se termine la estructura y se tenga que ajustar las etiquetas
+* por lo que sólo se necesita que se utilice la etiqueta que corresponde al momento actual.
+*/
+
+void ifthenelse_fin( FILE * fpasm, int etiqueta);
+/*
+* Generación de código para el fin de una estructura if-then-else
+* Como es el fin de uno bloque de control de flujo de programa que hace uso de la etiqueta
+* del mismo se requiere que antes de su invocación tome el valor de la etiqueta que le toca
+* según se ha explicado
+* Y tras ser invocada debe realizar el proceso para ajustar la información de las etiquetas
+* puesto que se ha liberado la última de ellas.
+*/
+
+void while_inicio(FILE * fpasm, int etiqueta);
+/*
+* Generación de código para el inicio de una estructura while
+* Como es el inicio de uno bloque de control de flujo de programa que requiere de una nueva
+* etiqueta deben ejecutarse antes las tareas correspondientes a esta situación
+* exp_es_variable
+* Es 1 si la expresión de la condición es algo asimilable a una variable (identificador,
+* elemento de vector)
+* Es 0 en caso contrario (constante u otro tipo de expresión)
+*/
+
+void while_exp_pila (FILE * fpasm, int exp_es_variable, int etiqueta);
+/*
+* Generación de código para el momento en el que se ha generado el código de la expresión
+* de control del bucle
+* Sólo necesita usar la etiqueta adecuada, por lo que sólo se necesita que se recupere el valor
+* de la etiqueta que corresponde al momento actual.
+* exp_es_variable
+* Es 1 si la expresión de la condición es algo asimilable a una variable (identificador,
+* o elemento de vector)
+* Es 0 en caso contrario (constante u otro tipo de expresión)
+*/
+
+void while_fin( FILE * fpasm, int etiqueta);
+/*
+* Generación de código para el final de una estructura while
+* Como es el fin de uno bloque de control de flujo de programa que hace uso de la etiqueta
+* del mismo se requiere que antes de su invocación tome el valor de la etiqueta que le toca
+* según se ha explicado
+* Y tras ser invocada debe realizar el proceso para ajustar la información de las etiquetas
+* puesto que se ha liberado la última de ellas.
+*/
 #endif
